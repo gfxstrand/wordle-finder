2a7e2cce8a3cc72a461b5dafdff47c9703722d86
Jason Ekstrand <jason.ekstrand@collabora.com>
2022-08-05 18:04:03 -0500

    Split out the main algorithm
    I want to try an alternate

--- src/main.rs
+++ src/main.rs
@@ -32,8 +32,68 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 struct Word {
-    index: u16,
+    word: String,
     letters: u32,
+}
+
+fn find_sets_of_5_dumb_loop(words: &Vec<Word>) {
+    let num_words = words.len();
+
+    let mut num_sets_2: u32 = 0;
+    let mut num_sets_3: u32 = 0;
+    let mut num_sets_4: u32 = 0;
+    let mut num_sets_5: u32 = 0;
+
+    for a in 0..num_words {
+        let wa = &words[a];
+        let letters1 = wa.letters;
+        for b in (a+1)..num_words {
+            let wb = &words[b];
+
+            if (letters1 & wb.letters) != 0 {
+                continue;
+            }
+            num_sets_2 += 1;
+            let letters2 = letters1 | wb.letters;
+
+            for c in (b+1)..num_words {
+                let wc = &words[c];
+
+                if (letters2 & wc.letters) != 0 {
+                    continue;
+                }
+                num_sets_3 += 1;
+                let letters3 = letters2 | wc.letters;
+
+                for d in (c+1)..num_words {
+                    let wd = &words[d];
+
+                    if (letters3 & wd.letters) != 0 {
+                        continue;
+                    }
+                    num_sets_4 += 1;
+                    let letters4 = letters3 | wd.letters;
+
+                    for e in (d+1)..num_words {
+                        let we = &words[e];
+
+                        if (letters4 & we.letters) != 0 {
+                            continue;
+                        }
+                        num_sets_5 += 1;
+
+                        println!("{}, {}, {}, {}, {}",
+                                 wa.word, wb.word, wc.word, wd.word, we.word);
+                    }
+                }
+            }
+        }
+    }
+    println!("Found {} words with unique letters", num_words);
+    println!("Found {} pairs of words with unique letters", num_sets_2);
+    println!("Found {} sets of three words with unique letters", num_sets_3);
+    println!("Found {} sets of four words with unique letters", num_sets_4);
+    println!("Found {} sets of five words with unique letters", num_sets_5);
 }
 
 fn main() {
@@ -41,98 +101,32 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file = File::open(&Path::new(&args[1])).unwrap();
 
-    // Read the file and filter to only 5-letter words
-    let lines = io::BufReader::new(file).lines();
-    let words: Vec<String> =
-        lines.map(|w| w.unwrap()).filter(|w| w.len() == 5).collect();
+    let mut words: Vec<Word> = Vec::new();
+    for line in io::BufReader::new(file).lines() {
+        let word = line.unwrap().trim().to_string();
+        if word.len() != 5 {
+            continue;
+        }
 
-    // The total number of words should fit in 16 letters
-    let num_words: u16 = words.len().try_into().unwrap();
-
-    // We only care about which letters are in a word, not the order of the
-    // letters.  This means, we can represent each word as a u32 with one bit
-    // set for each letter in the English alphabet.  This also makes it
-    // trivially easy to check if two words contain any of the same letters by
-    // using AND and to combine two words by using OR.
-    let mut singles: Vec<Word> = Vec::new();
-    for i in 0..num_words {
         let mut letters = 0u32;
-        for c in words[i as usize].to_uppercase().as_bytes() {
+        for c in word.to_uppercase().as_bytes() {
             assert!(b'A' <= *c && *c <= b'Z');
             letters |= 1u32 << (*c - b'A');
         }
-        if letters.count_ones() == 5 {
-            singles.push(Word {
-                index: i,
-                letters: letters,
-            });
+        if letters.count_ones() != 5 {
+            continue;
         }
+
+        words.push(Word {
+            word: word,
+            letters: letters,
+        });
     }
 
     // Get rid of any annograms.  This isn't necessary but cuts down on
     // runtime and lets us verify results against Matt's algorithm
-    singles.sort_by_key(|x| x.letters.reverse_bits());
-    singles.dedup_by_key(|x| x.letters);
+    words.sort_by_key(|w| w.letters.reverse_bits());
+    words.dedup_by_key(|w| w.letters);
 
-    let num_singles: u16 = singles.len().try_into().unwrap();
-
-    let mut num_sets_2: u32 = 0;
-    let mut num_sets_3: u32 = 0;
-    let mut num_sets_4: u32 = 0;
-    let mut num_sets_5: u32 = 0;
-
-    for a in 0..num_singles {
-        let sa = &singles[a as usize];
-        let letters1 = sa.letters;
-        for b in (a+1)..num_singles {
-            let sb = &singles[b as usize];
-
-            if (letters1 & sb.letters) != 0 {
-                continue;
-            }
-            num_sets_2 += 1;
-            let letters2 = letters1 | sb.letters;
-
-            for c in (b+1)..num_singles {
-                let sc = &singles[c as usize];
-
-                if (letters2 & sc.letters) != 0 {
-                    continue;
-                }
-                num_sets_3 += 1;
-                let letters3 = letters2 | sc.letters;
-
-                for d in (c+1)..num_singles {
-                    let sd = &singles[d as usize];
-
-                    if (letters3 & sd.letters) != 0 {
-                        continue;
-                    }
-                    num_sets_4 += 1;
-                    let letters4 = letters3 | sd.letters;
-
-                    for e in (d+1)..num_singles {
-                        let se = &singles[e as usize];
-
-                        if (letters4 & se.letters) != 0 {
-                            continue;
-                        }
-                        num_sets_5 += 1;
-
-                        println!("{}, {}, {}, {}, {}",
-                                 words[sa.index as usize],
-                                 words[sb.index as usize],
-                                 words[sc.index as usize],
-                                 words[sd.index as usize],
-                                 words[se.index as usize]);
-                    }
-                }
-            }
-        }
-    }
-    println!("Found {} words with unique letters", num_singles);
-    println!("Found {} pairs of words with unique letters", num_sets_2);
-    println!("Found {} sets of three words with unique letters", num_sets_3);
-    println!("Found {} sets of four words with unique letters", num_sets_4);
-    println!("Found {} sets of five words with unique letters", num_sets_5);
+    find_sets_of_5_dumb_loop(&words);
 }
